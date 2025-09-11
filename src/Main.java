e6882064e3c3eb0c4f297d28a4831d6d914905c6
Peter Olson <peter.olson@bfacademy.de>
2025-09-11 10:30:23 +0000

    Updates

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main
 {
     public static void main(String[] args){
-        System.out.println()
+       Microwave oven = new Microwave("Ikea", 1200);
+       Microwave oven2 = new Microwave("Sony", 400);
+        oven.cook30();
+
+        System.out.println(oven);
+        System.out.println(oven2);
+        
     }
 }
---
+++ src/Microwave.java
@@ -0,0 +1,53 @@
+public class Microwave{
+    //attributes
+    private String brand;
+    private int maxPower;
+
+    public Microwave (String manufacturer, int power)
+    {
+      
+        brand = manufacturer;
+        maxPower = power;
+    }
+    //microwave behaviors...
+    public void spin()
+    {
+
+    }
+    public void spin(boolean spinVal)
+    {
+
+    }
+    public void cook()
+    {
+
+    }
+    public void cook(int seconds, int power)
+    {
+        System.out.println("Cooking for " + seconds + " at " + power + "W");
+    }
+    public void cook30()
+    {
+        cook(30, maxPower);
+    }
+    public void beep()
+    {
+        System.out.println("beep");
+    }
+    public void defrost(int time)
+    {
+
+    }
+    public void defrost(int time, int power)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        Microwave mySweetCookingDevice = new Microwave("Samsung", 800);
+        mySweetCookingDevice.cook30();
+        mySweetCookingDevice.cook(120, 400);
+        mySweetCookingDevice.beep();
+    }
+}
